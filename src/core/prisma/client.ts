263945b8c775720e21a69c8da97af882317faff8
javierlnmn <jlm03123@gmail.com>
2026-04-27 20:55:00 +0200

    chore(prisma): update required env vars

--- src/core/prisma/client.ts
+++ src/core/prisma/client.ts
@@ -1,13 +1,15 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { env } from "../config/env.js";
 import { PrismaClient } from "./generated/client";
 
 const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 5,
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  user: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_NAME,
+  connectionLimit: 5,
 });
 const prisma = new PrismaClient({ adapter });
 
