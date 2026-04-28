2259649bccedf84c2f81f1b8474f5df78c0e96ae
javierlnmn <jlm03123@gmail.com>
2026-04-28 15:42:18 +0200

    fix(prisma client): parse database port to number

--- src/core/prisma/client.ts
+++ src/core/prisma/client.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "./generated/client";
 
 const adapter = new PrismaMariaDb({
   host: env.DATABASE_HOST,
-  port: env.DATABASE_PORT,
+  port: Number(env.DATABASE_PORT),
   user: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
