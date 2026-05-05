e9b3799777117b51eaf23aad341ace4d129c38d1
javierlnmn <jlm03123@gmail.com>
2026-05-05 14:29:23 +0200

    feat(prisma): create and apply first migration to dev database

---
+++ prisma/migrations/20260505120515_initial/migration.sql
@@ -0,0 +1,124 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `surname` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `user_type` ENUM('creator', 'sponsor') NOT NULL,
+    `enabled` BOOLEAN NOT NULL DEFAULT true,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `events` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `cover_image` VARCHAR(191) NULL,
+    `date` DATETIME(3) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `industry_field` VARCHAR(191) NOT NULL,
+    `expected_attendance` INTEGER NOT NULL,
+    `tags` VARCHAR(191) NULL,
+    `event_creator_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `default_tiers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `benefits` VARCHAR(191) NOT NULL,
+    `event_creator_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `event_tiers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `benefits` VARCHAR(191) NOT NULL,
+    `tier_creator_id` INTEGER NOT NULL,
+    `event_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `proposals` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tier_name` VARCHAR(191) NOT NULL,
+    `tier_price` DECIMAL(10, 2) NOT NULL,
+    `tier_benefits` VARCHAR(191) NOT NULL,
+    `is_event_tier` BOOLEAN NOT NULL,
+    `status` ENUM('NEGOTIATING', 'COUNTER_OFFERED', 'ACCEPTED', 'DECLINED') NOT NULL DEFAULT 'NEGOTIATING',
+    `event_tier_id` INTEGER NULL,
+    `sponsor_id` INTEGER NOT NULL,
+    `event_creator_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `conversations` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `event_creator_id` INTEGER NOT NULL,
+    `event_sponsor_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `conversations_event_creator_id_event_sponsor_id_key`(`event_creator_id`, `event_sponsor_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `messages` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `message` TEXT NOT NULL,
+    `last_read` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `conversation_id` INTEGER NOT NULL,
+    `sender_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `events` ADD CONSTRAINT `events_event_creator_id_fkey` FOREIGN KEY (`event_creator_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `default_tiers` ADD CONSTRAINT `default_tiers_event_creator_id_fkey` FOREIGN KEY (`event_creator_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `event_tiers` ADD CONSTRAINT `event_tiers_tier_creator_id_fkey` FOREIGN KEY (`tier_creator_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `event_tiers` ADD CONSTRAINT `event_tiers_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `proposals` ADD CONSTRAINT `proposals_event_tier_id_fkey` FOREIGN KEY (`event_tier_id`) REFERENCES `event_tiers`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `proposals` ADD CONSTRAINT `proposals_sponsor_id_fkey` FOREIGN KEY (`sponsor_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `proposals` ADD CONSTRAINT `proposals_event_creator_id_fkey` FOREIGN KEY (`event_creator_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `conversations` ADD CONSTRAINT `conversations_event_creator_id_fkey` FOREIGN KEY (`event_creator_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `conversations` ADD CONSTRAINT `conversations_event_sponsor_id_fkey` FOREIGN KEY (`event_sponsor_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `messages` ADD CONSTRAINT `messages_conversation_id_fkey` FOREIGN KEY (`conversation_id`) REFERENCES `conversations`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `messages` ADD CONSTRAINT `messages_sender_id_fkey` FOREIGN KEY (`sender_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
