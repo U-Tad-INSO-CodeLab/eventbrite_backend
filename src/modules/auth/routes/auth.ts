0aa871b7e23f6ffcce641ac4217e15a569f4c584
javierlnmn <jlm03123@gmail.com>
2026-05-05 16:09:28 +0200

    fix(auth): fix router name

--- src/modules/auth/routes/auth.ts
+++ src/modules/auth/routes/auth.ts
@@ -4,12 +4,12 @@ import { getProfile } from "@/modules/auth/controllers/profile";
 import { authMiddleware } from "@/modules/auth/middleware/auth";
 import { UserType } from "@/core/prisma/generated/enums";
 
-const router = express.Router();
+const authRouter = express.Router();
 
 for (const user_type of Object.values(UserType)) {
-  router.post(`/register/${user_type}`, register);
+  authRouter.post(`/register/${user_type}`, register);
 }
-router.post("/login", login);
-router.get("/profile", authMiddleware, getProfile);
+authRouter.post("/login", login);
+authRouter.get("/profile", authMiddleware, getProfile);
 
-export default router;
+export default authRouter;
