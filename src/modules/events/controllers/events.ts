663dd0c3882c7b02b0c82e9b966450e6fc834c28
pepealacid <ppalacid@gmail.com>
2026-05-13 20:43:37 +0200

    refactor: build where util extracted to folder

--- src/modules/events/controllers/events.ts
+++ src/modules/events/controllers/events.ts
@@ -1,5 +1,4 @@
 import { prisma } from "@/core/prisma/client";
-import { Prisma } from "@/core/prisma/generated/client";
 import { getContextUser } from "@/modules/auth/utils/context";
 import { EVENT_TAG_OPTIONS } from "@/modules/events/constants/eventTags";
 import { EVENT_UPLOAD_SUBDIR } from "@/modules/events/middleware/uploadEventCover";
@@ -10,50 +9,9 @@ import {
 import { Request, Response } from "express";
 import path from "path";
 import status from "http-status";
+import { buildSponsorEventWhere } from "../utils/buildSponsorEventWhere";
 
-function buildSponsorEventWhere(
-  filters: SponsorEventsQuery,
-): Prisma.EventWhereInput {
-  const where: Prisma.EventWhereInput = {
-    published: true,
-  };
 
-  if (filters.target_amount_from != null || filters.target_amount_to != null) {
-    where.target_amount = {};
-    if (filters.target_amount_from != null) {
-      where.target_amount.gte = filters.target_amount_from;
-    }
-    if (filters.target_amount_to != null) {
-      where.target_amount.lte = filters.target_amount_to;
-    }
-  }
-
-  if (filters.audience_from != null || filters.audience_to != null) {
-    where.expected_attendance = {};
-    if (filters.audience_from != null) {
-      where.expected_attendance.gte = filters.audience_from;
-    }
-    if (filters.audience_to != null) {
-      where.expected_attendance.lte = filters.audience_to;
-    }
-  }
-
-  if (filters.location !== "all") {
-    where.location = filters.location;
-  }
-
-  if (filters.industry !== "all") {
-    where.industry_field = filters.industry;
-  }
-
-  if (filters.tags.length > 0) {
-    where.AND = filters.tags.map((tag) => ({
-      tags: { contains: tag },
-    }));
-  }
-
-  return where;
-}
 
 export const createEvent = async (req: Request, res: Response) => {
   const event_creator_id = getContextUser()!.id;
